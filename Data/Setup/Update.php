ce5508873eca9be4b1919b26e209386724725328
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-10-04 08:55:08 +0200

    update now remove the no longer needed /Data/Help directory

--- Data/Setup/Update.php
+++ Data/Setup/Update.php
@@ -15,9 +15,23 @@ use Silex\Application;
 
 class Update
 {
+    protected $app = null;
+
+    /**
+     * Release 0.16
+     */
+    protected function release_016()
+    {
+        // the helpfile graphics are moved to the 'PublicGraphics' repository
+        $this->app['filesystem']->remove(MANUFAKTUR_PATH.'/CommandCollection/Data/Help');
+    }
 
     public function exec(Application $app)
     {
+        $this->app = $app;
+
+        // Release 0.16
+        $this->release_016();
 
         return $app['translator']->trans('Successfull updated the extension %extension%.',
             array('%extension%' => 'CommandCollection'));
